39d1f47d8d570dadfcb5e48d929f1882380db098
miraz <miraz@nvisio.net>
2017-09-24 12:16:11 +0600

    Add Classs and Set up Db

---
+++ WorkForceApp/WorkForceApp/Model/TaskType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkForceApp.Model
+{
+    class TaskType
+    {
+        public int type_id { get; set; }
+        public string name { get; set; }
+    }
+}
